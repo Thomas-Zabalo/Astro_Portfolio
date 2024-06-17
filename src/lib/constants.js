c77545211022673e53348d476d38fae8a556a075
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-06-17 15:21:25 +0200

    ASTRO

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -5,3 +5,70 @@ export const LINKS = {
   instagram: "https://www.instagram.com/thomas.zab",
   discord: "https://discordapp.com/users/rubinoki",
 };
+
+export const technologies = [
+	{ name: "JavaScript", icon: "mdi:language-javascript" },
+	{ name: "React", icon: "mdi:react" },
+	{ name: "React Native", icon: "tabler:brand-react-native" },
+	{ name: "Next", icon: "ri:nextjs-fill" },
+	{ name: "Astro", icon: "tabler:brand-astro" },
+	{ name: "PHP", icon: "mdi:language-php" },
+	{ name: "Laravel", icon: "mdi:laravel" },
+	{ name: "NodeJS", icon: "mdi:nodejs" },
+	{ name: "SQL", icon: "mdi:sql-query" },
+	{ name: "Tailwind", icon: "mdi:tailwind" },
+];
+export const works = [
+	{
+		title: "Application web PHP",
+		link: "https://github.com/mmicastres/sae301-Thomas-Zabalo",
+		image:"/public/assets/MoodleMMI.png",
+		description:
+			"Création d'une application web en PHP. Permet au utilisateur d'ajouter, commenter des projets",
+		langage: "PHP | TWIG",
+		icon: "mdi:github",
+	},
+	{
+		title: "Application web React",
+		link: "https://github.com/Thomas-Zabalo/React",
+		image:"/public/assets/BGWeb.png",
+		description:
+			"Création d'une application web en React. Permet au utilisateur de créer un personnage du jeu Baldur Gate 3",
+		langage: "REACT | LARAVEL",
+		icon: "mdi:github",
+	},
+	{
+		title: "Application Mobile",
+		link: "https://github.com/Thomas-Zabalo/React-native",
+		image:"/public/assets/BGMobile.png",
+		description:
+			"Création d'une application mobile en React Native. Permet au utilisateur de créer un personnage du jeu Baldur Gate 3",
+		langage: "REACT NATIVE | EXPO | API",
+		icon: "mdi:github",
+	},
+	{
+		title: "TO DO LIST",
+		link: "https://github.com/Thomas-Zabalo/TO-DO-LIST",
+		image:"/public/assets/TODO.png",
+		description: "Création d'une application web pour organiser mes journées et mes tâches",
+		langage: "NODE JS",
+		icon: "mdi:github",
+	},
+	{
+		title: "Jeux Web",
+		link: "https://github.com/Thomas-Zabalo/Phaser",
+		image:"/public/assets/Cyberpsychose.png",
+		description: "Création d'un jeu en 2D avec le moteur de rendu Phaser",
+		langage: "PHASER | JS",
+		icon: "mdi:github",
+	},
+	{
+		title: "Call API",
+		link: "https://github.com/mmicastres/s3-sae-thomas-etienne-florestan",
+		image:"/public/assets/Ghibli.png",
+		description:
+			"Création d'un applicaiton web sur le monde de GHIBLI pour permettre au utilisateur de voir les avis des films",
+		langage: "JS | API",
+		icon: "mdi:github",
+	},
+];
